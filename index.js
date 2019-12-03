ffea18016508bd14e7ecf66a4e97224e873a0606
Hunter Evanoff <hunter547@gmail.com>
2019-12-02 21:29:19 -0500

    Set up GET, PUT, POST, and DELETE requests for the API and started documenting how to use it
    Set up various CRUD requests and documented how to use their endpoints in documentation.html. See the page for more details. Created GET requests for movies, genres, directors. Created POST requests for new users and adding movies to favorites. Created a PUT request for updating a user. Created DELETE requests for removing a movie from favorites and deregistering a user.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express'),
-  morgan = require('morgan');
+  morgan = require('morgan'),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
 const app = express();
 
 app.use(morgan('common'));
+app.use(bodyParser.json());
 
 let bestMovies = [{
   movie : 'Pulp Fiction'
@@ -36,6 +39,8 @@ let bestMovies = [{
   movie : 'The Conjuring'
 }];
 
+var users = [{ name: 'John Smith' }];
+
 // GET requests
 app.get('/', function(req, res) {
   res.send('Welcome to my movie api!')
@@ -46,6 +51,50 @@ app.get('/documentation', function(req, res) {
 app.get('/movies', function(req, res) {
   res.json(bestMovies);
 });
+app.get('/users', function(req, res) {
+  res.json(users);
+})
+app.get('/movies/:name', function(req, res) {
+  res.json(bestMovies.find(( movie ) =>
+    { return movie.movie === req.params.name }));
+});
+app.get('/genres/:name', function(req, res) {
+  res.send('Request for information about the ' + req.params.name + ' genre, which explains its relative description and movies.');
+});
+app.get('/directors/:name', function(req, res) {
+  res.send('Information about director ' + req.params.name + '\'s bio, birth year, and death year (if applicable) has been requested.');
+});
+
+// POST requests
+app.post('/users/new/:name', function(req, res) {
+    var newUser = { name : req.params.name };
+    users.push(newUser);
+    res.send('Created a new user ' + req.params.name);
+});
+app.post('/movies/new/:name', function(req, res) {
+  res.send('New movie "' + req.params.name + '" was added to your favorites.');
+});
+
+// PUT requests
+app.put('/users/update/:name', function(req, res) {
+  res.send('Updated user ' + req.params.name + ' successfully.');
+});
+app.put('/users/update/:oldname/:newname', function(req, res) {
+  users.find(( user ) => {
+    if (user.name === req.params.oldname) {
+      user.name = req.params.newname;
+    }});
+    res.send('User formally known as ' + req.params.oldname + ', is now ' + req.params.newname);
+  });
+
+// DELETE requests
+app.delete('/movies/delete/:name', function(req, res) {
+  res.send('The movie "' + req.params.name + '" was removed from your favorites.');
+});
+app.delete('/users/delete/:name', function(req, res) {
+  res.send('The user ' + req.params.name + ' has been deregistered.')
+});
+
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
