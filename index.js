3a0f079e6e56aa1bf53e569f29ca360dff23290d
Hunter Evanoff <hunter547@gmail.com>
2020-02-14 19:43:19 -0500

    Made an error adjustment to fix a missing bracket.

--- index.js
+++ index.js
@@ -300,59 +300,60 @@ app.put('/users/:username', passport.authenticate('jwt', { session: false }),
       .catch(function(err) {
         console.error(err);
         res.status(500).send("Error: " + err);
-      });
+      })
+  });
 
-    // DELETE requests
-    app.delete('/users/:Username/Movies/:MovieID', passport.authenticate('jwt', { session: false }), function(req, res) {
-      Users.findOneAndUpdate({ username: req.params.Username }, {
-        $pull: { favorite_movies: req.params.MovieID }
-      }).populate('favorite_movies', 'title')
-        .exec(function(err, user) {
-          if (err) {
-            console.error(err);
-            res.status(400).send("Error: " + err);
-          }
-          if (user) {
-            var foundMovie = 0;
-            user.favorite_movies.forEach(favorite_movie => {
-              if (favorite_movie._id == req.params.MovieID) {
-                foundMovie = 1;
-                res.status(200).send(favorite_movie.title
-                  + ' was removed from ' + req.params.Username + '\'s favorites.');
-              }
-            })
-            if (foundMovie === 0) {
-              res.status(400).send(req.params.MovieID + ' was not found in ' + req.params.Username + '\'s favorites.');
-            }
-          } else {
-            res.status(400).send(req.params.MovieID + ' was not found in ' + req.params.Username + '\'s favorites.');
+// DELETE requests
+app.delete('/users/:Username/Movies/:MovieID', passport.authenticate('jwt', { session: false }), function(req, res) {
+  Users.findOneAndUpdate({ username: req.params.Username }, {
+    $pull: { favorite_movies: req.params.MovieID }
+  }).populate('favorite_movies', 'title')
+    .exec(function(err, user) {
+      if (err) {
+        console.error(err);
+        res.status(400).send("Error: " + err);
+      }
+      if (user) {
+        var foundMovie = 0;
+        user.favorite_movies.forEach(favorite_movie => {
+          if (favorite_movie._id == req.params.MovieID) {
+            foundMovie = 1;
+            res.status(200).send(favorite_movie.title
+              + ' was removed from ' + req.params.Username + '\'s favorites.');
           }
         })
-    });
+        if (foundMovie === 0) {
+          res.status(400).send(req.params.MovieID + ' was not found in ' + req.params.Username + '\'s favorites.');
+        }
+      } else {
+        res.status(400).send(req.params.MovieID + ' was not found in ' + req.params.Username + '\'s favorites.');
+      }
+    })
+});
 
-    app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), function(req, res) {
-      Users.findOneAndRemove({ username: req.params.Username })
-        .then(function(user) {
-          if (!user) {
-            res.status(400).send(req.params.Username + " was not found");
-          } else {
-            res.status(200).send(req.params.Username + " was deleted.");
-          }
-        })
-        .catch(function(err) {
-          console.error(err);
-          res.status(500).send("Error: " + err);
-        });
+app.delete('/users/:Username', passport.authenticate('jwt', { session: false }), function(req, res) {
+  Users.findOneAndRemove({ username: req.params.Username })
+    .then(function(user) {
+      if (!user) {
+        res.status(400).send(req.params.Username + " was not found");
+      } else {
+        res.status(200).send(req.params.Username + " was deleted.");
+      }
+    })
+    .catch(function(err) {
+      console.error(err);
+      res.status(500).send("Error: " + err);
     });
+});
 
 
-    app.use(function(err, req, res, next) {
-      console.error(err.stack);
-      res.status(500).send('Error, please read the console for more details');
-    });
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Error, please read the console for more details');
+});
 
-    // listen for requests
-    var port = process.env.PORT || 3000;
-    app.listen(port, "0.0.0.0", function() {
-      console.log("Listening on Port 3000");
-    });
+// listen for requests
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+  console.log("Listening on Port 3000");
+});
